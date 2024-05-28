80629603dae71a83c27078825dfcde5a2fd5a74d
herulobarto <herulobarto@gmail.com>
2024-05-28 13:07:48 +0700

    autentikasi authcontroller

--- go-auth/main.go
+++ go-auth/main.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	authcontroller "github.com/herulobarto/go-auth/controllers"
+)
 
 func main() {
 
-	fmt.Println("Hello world")
+	http.HandleFunc("/", authcontroller.Index)
+
+	fmt.Println("server jalan di: http://localhost:3000")
+	http.ListenAndServe(":3000", nil)
 }
