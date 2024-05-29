7189e75af95003b85a245d9f621d6dfe90f92794
herulobarto <herulobarto@gmail.com>
2024-05-29 07:00:22 +0700

    buat variable usermodel dan install go crypto bycript

--- go-auth/controllers/authcontroler.go
+++ go-auth/controllers/authcontroler.go
@@ -1,14 +1,20 @@
 package controllers
 
 import (
+	"errors"
 	"html/template"
 	"net/http"
+
+	"github.com/herulobarto/go-auth/entities"
+	"github.com/herulobarto/go-auth/models"
 )
 
 type UserInput struct {
 	Username string
 	Password string
 }
+
+var UserModel = models.NewUserModel()
 
 func Index(w http.ResponseWriter, r *http.Request) {
 
@@ -29,6 +35,20 @@ func Login(w http.ResponseWriter, r *http.Request) {
 			Username: r.Form.Get("username"),
 			Password: r.Form.Get("password"),
 		}
+
+		var user entities.User
+		userModel.where(&user, "username", UserInput.Username)
+
+		var message error
+		if user.Username == "" {
+			// tidak ditemukan di database
+			message = errors.New("Username atau Password salah!")
+		} else {
+			// pengecekan password
+		}
+
 	}
 
 }
+
+// lalu ketik pada terminal go get golang.org.x/crypto/bcrypt
